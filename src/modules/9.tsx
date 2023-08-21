d4ea66c44fd75c83f2f3eb3e1d767c47bf2cbd37
leo.araujo <leo.araujo@seap.ma.gov.br>
2023-08-21 17:25:40 -0300

    dia 21 ok

---
+++ src/modules/9.tsx
@@ -0,0 +1,62 @@
+/* eslint-disable react/no-unescaped-entities */
+import TabPane from "antd/lib/tabs/TabPane";
+import { SubTitulo, Texto, Titulo } from "./styles";
+import Oracao from "./oracao";
+
+export default function Dia9() {
+  return (
+    <>
+      <br />
+      <Titulo style={{ textAlign: "center" }}>Dia 23 de Agosto</Titulo>
+      <br />
+      <Oracao />
+      <br />
+      <br />
+      {/* <Titulo style={{ textAlign: "center" }}>Evangelho: Mt 19,16-22</Titulo>
+      <Texto style={{ textAlign: "center" }}>
+        Naquele tempo, alguém aproximou-se de Jesus e disse: "Mestre, o que devo
+        fazer de bom para possuir a vida eterna?" <br />
+        Jesus respondeu: "Por que tu me perguntas sobre o que é bom? Um só é o
+        Bom. <br />
+        Se tu queres entrar na vida, observa os mandamentos". <br />O homem
+        perguntou: "Quais mandamentos?" <br />
+        Jesus respondeu: "Não matarás, não cometerás adultério, não roubarás,
+        não levantarás falso testemunho, honra teu pai e tua mãe, e ama teu
+        próximo como a ti mesmo". <br />O jovem disse a Jesus: "Tenho observado
+        todas essas coisas. O que ainda me falta?" <br />
+        Jesus respondeu: "Se tu queres ser perfeito, vai, vende tudo o que tens,
+        dá o dinheiro aos pobres e terás um tesouro no céu. <br />
+        Depois, vem e segue-me". Quando ouviu isso, o jovem foi embora cheio de
+        tristeza, porque era muito rico. <br />
+        <br />
+        Palavra da Salvação, glória a vós Senhor.
+        <br />
+        <br />
+      </Texto> */}
+      {/* <SubTitulo style={{ textAlign: "center" }}>Para meditar:</SubTitulo>
+      <Texto style={{ textAlign: "center" }}>
+        O texto aborda a solenidade da Assunção de Nossa Senhora, um momento
+        importante na vida de Maria, em que ela foi elevada ao céu de corpo e
+        alma, recebendo a recompensa prometida àqueles que vivem em Cristo.{" "}
+        <br /> A morte é considerada o último inimigo a ser destruído,
+        consequência do pecado, e todos morremos devido à natureza decaída.{" "}
+        <br /> A opção de seguir Cristo é destacada como o caminho para a
+        ressurreição. <br /> Paulo fala sobre os que estão em Cristo e morrem,
+        ressuscitando no último dia, quando Cristo retornar. <br /> A morte
+        também é vista como a vinda de Cristo para cada indivíduo. <br /> Nossa
+        Senhora é ressaltada como imaculada, sem a consequência do pecado, e sua
+        Assunção é explicada como uma ascensão de corpo e alma, resultando na
+        plenitude da ressurreição. <br /> O texto afirma que aqueles que morrem
+        não estão dormindo, mas vivos em Deus, cada um na glória ou no castigo,
+        aguardando a ressurreição com o corpo glorioso no último dia. <br />{" "}
+        Nossa Senhora e outros santos intercedem por nós, mas apenas Maria já
+        possui o corpo glorioso, enquanto os outros o terão na ressurreição.{" "}
+        <br />
+        A ressurreição é vista como um retorno glorioso ou um destino
+        indesejável, dependendo das ações em vida. <br />
+        <br />
+      </Texto> */}
+      <br />
+    </>
+  );
+}
